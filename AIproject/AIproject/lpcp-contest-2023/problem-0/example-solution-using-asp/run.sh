895153438752a02badc3d85ec61695bd5ec0056d
Mathias Innleggen Vestergaard <mathiasvestergaard@MacBook-Air-tilhrende-Mathias.local>
2025-06-21 17:21:26 +0200

    ci: finalize workflow and contest‐checker import

--- AIproject/AIproject/lpcp-contest-2023/problem-0/example-solution-using-asp/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# example usage:
-# $ ./run.sh <../instance.1.in
-
-DIRNAME=`dirname $0`
-
-python3 "$DIRNAME/input.py" | clingo - "$DIRNAME/encoding.lp" | python3 "$DIRNAME/output.py"
-
